ccf2385b8269f02e4308e560017c3e27a246e637
Linda-Nicky <ohayaghalinda.c@gmail.com>
2023-02-22 07:53:16 -0800

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void postive_or _negative(int i);
+int largest_number(int a,int b, int c);
+void print_remaining_dyas(int day, int month, int day);
+int convert_dat(int month, int day);
+
+
+#endif
