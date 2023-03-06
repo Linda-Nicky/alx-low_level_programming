d88d84bd6f301dd9dfd80152218dc4bed5414d88
Linda-Nicky <ohayaghalinda.c@gmail.com>
2023-03-06 06:50:54 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - A function that prints the chessboard.
+ * @a: two dimension array to print (2D).
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j; /* variables*/
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
