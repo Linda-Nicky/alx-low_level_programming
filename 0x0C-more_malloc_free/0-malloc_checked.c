1b5ae8bee7f879377eae46368ae6ab5b39bee7bd
Linda-Nicky <ohayaghalinda.c@gmail.com>
2023-03-16 08:02:20 -0700

    0-malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *mem;
+	void *ptr;
 
-	mem = malloc(b);
-	if (mem == NULL)
+	ptr = malloc(b);
+	if (ptr == NULL)
 		exit(98);
-	return (mem);
+	return (ptr);
 }
