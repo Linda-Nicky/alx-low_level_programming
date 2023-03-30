a0dc0d639c878df8871c3a799052304f866c602b
Linda-Nicky <ohayaghalinda.c@gmail.com>
2023-03-30 08:31:30 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * beforemain - a function that runs before main
+ *
+ * Return: nothing
+ */
+
+void __attribute__ ((constructor)) beforemain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
