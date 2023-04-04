8ff067e1a435d353dd9f32c694addd7c1e612ade
Linda-Nicky <ohayaghalinda.c@gmail.com>
2023-04-04 01:26:52 -0700

    1-listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * listint_len - a function that returns the number of elements
+ * in a linked listint_t list.
+ * @h: the head (first node) of the listint_t list.
+ *
+ * Return: the total number of elements present in the listint_t list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t element = 0;
+	const listint_t *ptr = h;
+
+	while (ptr)
+	{
+		/**
+		 * increment
+		 */
+
+		element = element + 1;
+		printf("%d/n", ptr->n);
+		ptr = ptr->next;
+	}
+
+	return (element);
+}
